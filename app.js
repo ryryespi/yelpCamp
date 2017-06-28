1d891447fc1b0721b8f5f4cd2fa34bde864a0562
Ryan Espinosa <ryryespi@yahoo.com>
2017-06-28 00:30:53 +0000

    added comment logic

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ var express     = require("express"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
+    methodOverride = require("method-override"),
     seedDB      = require("./seeds");
 
-
+var commentRoutes        = require("./routes/comments"),
+    campgroundRoutes     = require("./routes/campgrounds"),
+    indexRoutes          = require("./routes/index");
 
 app.set("view engine", "ejs");
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 seedDB();
 
 
@@ -40,152 +44,25 @@ app.use(function(req, res, next){
     next();
 });
 
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 app.get("/",function(req, res){
     res.render("landing");
 });
 
 
-Campground.create(
-    {name: "Mountain Goat's Rest",
-    image: "https://www.nps.gov/shen/planyourvisit/images/Campgrounds_1.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ligula leo, porttitor eu risus auctor, dignissim vehicula lacus. Cras vitae blandit libero. Sed sit amet neque ac enim rutrum sodales. Ut lorem augue, bibendum quis luctus scelerisque, tristique id eros. Etiam vitae sapien neque. Maecenas tempus diam ut sagittis tincidunt. Vivamus fringilla ipsum enim, sed aliquet metus tempus eu."
-    },
-    
-    function(err, campground){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("NEWLY CREATED CAMPGROUND ");
-            console.log(campground);
-        }
-    });
 
 
-//Index - show all campgrounds
-app.get("/campgrounds", function(req, res){
-    //Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("campgrounds/index",{campgrounds:allCampgrounds});
-        }
-    });
-    
-});
-
-app.post("/campgrounds", function(req, res){
-    //get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
-    //create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        }else{
-            res.redirect("/campgrounds");
-        }
-    });
-});   
-
-app.get("/campgrounds/new", function(req,res){
-    res.render("campgrounds/new");
-});
-
-//SHOW
-app.get("/campgrounds/:id", function(req,res){
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-       if(err){
-           console.log(err);
-       } else{
-           res.render("campgrounds/show", {campground: foundCampground});
-       }
-    });
-    //req.params.id
-});
-
-//Comments
-
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req,res){
-    Campground.findById(req.params.id, function(err, campground){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("comments/new",{campground: campground});
-        }
-    });
-});
-
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
-    //lookup campground using ID
-    Campground.findById(req.params.id, function(err,campground){
-        if(err){
-            console.log(err);
-            res.redirect("/campgrounds");
-        }else{
-            Comment.create(req.body.comment,function(err, comment){
-                if(err){
-                    console.log(err);
-                }else{
-                    campground.comments.push(comment);
-                    campground.save();
-                    res.redirect("/campgrounds/" + campground._id);
-                }
-            });
-        }
-    });
-    //create new
-});
-
-// AUTH ROUTES
-//show register form
-app.get("/register", function(req,res){
-    res.render("register");
-});
-
-//handle sign up logic
-app.post("/register", function(req,res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err,user){
-        if(err){
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req,res, function(){
-            res.redirect("/campgrounds");
-        });
-    });
-});
-
-//show login form
-app.get("/login", function(req,res){
-    res.render("login");
-});
-
-//handling login logic
-app.post("/login", passport.authenticate("local", 
-{ 
-    successRedirect: "/campgrounds",
-    failureRedirect: "/login"
-}), function(req,res){
-});
-
-//loq out route
-app.get("/logout", function(req,res){
-    req.logout();
-    res.redirect("/campgrounds");
-});
 
 
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-        res.redirect("/login");
-    
-}
+//edit campground route
+//router.get("/:id/edit", function(req, res){
+ //   res.send("EDIT CAMPGROUND ROUTE");
+//});
+//update campground route
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Server has Started!");
---
+++ routes/campgrounds.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router = express.Router({mergeParams:true});
+var Campground = require("../models/campground");
+
+Campground.create(
+    {name: "Mountain Goat's Rest",
+    image: "https://www.nps.gov/shen/planyourvisit/images/Campgrounds_1.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ligula leo, porttitor eu risus auctor, dignissim vehicula lacus. Cras vitae blandit libero. Sed sit amet neque ac enim rutrum sodales. Ut lorem augue, bibendum quis luctus scelerisque, tristique id eros. Etiam vitae sapien neque. Maecenas tempus diam ut sagittis tincidunt. Vivamus fringilla ipsum enim, sed aliquet metus tempus eu."
+    },
+    
+    function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("NEWLY CREATED CAMPGROUND ");
+            console.log(campground);
+        }
+    });
+
+
+
+
+
+//Index - show all campgrounds
+router.get("/", function(req, res){
+    //Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds/index",{campgrounds:allCampgrounds});
+        }
+    });
+    
+});
+
+router.post("/", isLoggedIn, function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
+    //create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
+});   
+
+router.get("/new", isLoggedIn, function(req,res){
+    res.render("campgrounds/new");
+});
+
+//SHOW
+router.get("/:id", function(req,res){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+       if(err){
+           console.log(err);
+       } else{
+           res.render("campgrounds/show", {campground: foundCampground});
+       }
+    });
+    //req.params.id
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+        res.redirect("/login");
+    
+}
+
+module.exports = router;
