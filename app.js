4211234acca188c059797397240d4adc55f54e92
Ryan Espinosa <ryryespi@yahoo.com>
2017-07-05 20:27:57 +0000

    added ports and ip's

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 8081, process.env.IP || "0.0.0.0", function(){
    console.log("The YelpCamp Server Has Started!");
 });
